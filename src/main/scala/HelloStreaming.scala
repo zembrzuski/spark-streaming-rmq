d784583b1ac5127ed92e989eed494ad4a70a5729
Your Name <you@example.com>
2017-03-12 18:08:28 -0300

    RabbitMQ connection - does not work yet

--- src/main/scala/HelloStreaming.scala
+++ src/main/scala/HelloStreaming.scala
@@ -1,5 +1,7 @@
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.rabbitmq.RabbitMQUtils
+import org.apache.spark.streaming.rabbitmq.distributed.RabbitMQDistributedKey
+import org.apache.spark.streaming.rabbitmq.models.ExchangeAndRouting
 import org.apache.spark.{SparkConf, SparkContext}
 
 object HelloStreaming {
@@ -9,11 +11,25 @@ object HelloStreaming {
     val conf = new SparkConf().setAppName("streaming").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(5))
-    val lines = ssc.socketTextStream("localhost", 7777)
-    lines.print()
 
-    ssc.start()
-    ssc.awaitTermination()
+    val queue = "spark-hello"
+    val exchangeName = Option.apply("spark-hello")
+    val exchangeType = Option.apply("direct")
+    val routingKey = Option.apply("spark-hello")
+    val exchangeAndRouting = ExchangeAndRouting(exchangeName, exchangeType, routingKey)
+    val connectionParams = Map("TODO" -> "DO-IT")
+    val rmqDistributedKey = RabbitMQDistributedKey(queue, exchangeAndRouting, connectionParams)
+    val distributedKeys = Seq(rmqDistributedKey)
+    val rabbitMQParams = Map.empty[String, String]
+
+    val receiverStream = RabbitMQUtils.createDistributedStream[String](ssc, distributedKeys, rabbitMQParams)
+
+    //    basic example.
+//   ----------------
+//    val lines = ssc.socketTextStream("localhost", 7777)
+//    lines.print()
+//    ssc.start()
+//    ssc.awaitTermination()
 
   }
 
