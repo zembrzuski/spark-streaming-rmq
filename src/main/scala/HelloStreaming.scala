d0edf0b2ee549413674407e5e8f9d2d69a5504ba
Your Name <you@example.com>
2017-03-12 16:57:38 -0300

    rmq dependency working + simple streaming

--- src/main/scala/HelloStreaming.scala
+++ src/main/scala/HelloStreaming.scala
@@ -1,11 +1,19 @@
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.rabbitmq.RabbitMQUtils
 import org.apache.spark.{SparkConf, SparkContext}
 
 object HelloStreaming {
 
   def main(args: Array[String]): Unit = {
 
-    val sc = new SparkContext(new SparkConf().setAppName("hello-spark").setMaster("local[*]"))
-    print("hello scala")
+    val conf = new SparkConf().setAppName("streaming").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(5))
+    val lines = ssc.socketTextStream("localhost", 7777)
+    lines.print()
+
+    ssc.start()
+    ssc.awaitTermination()
 
   }
 
